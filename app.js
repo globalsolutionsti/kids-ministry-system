2586d0916cdab59630672365fc8d4c366f904f97
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-04-04 17:53:17 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,50 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbxrEdnEyWFd2COYnVfOvJjkODstQWy_BMcptioVIAo0E0n4005GIBPYTz8PGi3JnR2R/exec";
+
+// LOGIN
+function login() {
+  fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      action: "login",
+      username: document.getElementById("username").value,
+      password: document.getElementById("password").value
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.success) {
+      document.getElementById("login").classList.add("hidden");
+      document.getElementById("app").classList.remove("hidden");
+    } else {
+      alert("Credenciales incorrectas");
+    }
+  });
+}
+
+// AGREGAR NIÑO
+function addKid() {
+  fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      action: "addKid",
+      name: document.getElementById("kidName").value,
+      photo: document.getElementById("kidPhoto").value
+    })
+  })
+  .then(res => res.json())
+  .then(() => alert("Niño guardado"));
+}
+
+// AGREGAR ADULTO
+function addAdult() {
+  fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      action: "addAdult",
+      name: document.getElementById("adultName").value,
+      phone: document.getElementById("adultPhone").value
+    })
+  })
+  .then(res => res.json())
+  .then(() => alert("Adulto guardado"));
+}
